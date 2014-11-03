b216fd26bb7d458df1a5482ee9dab076d8f9660f
penniath <elfari69@gmail.com>
2014-11-03 20:27:46 +0100

    Estaticos

--- eventus/apps/events/views.py
+++ eventus/apps/events/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Event, Category
 
+
 def index(request):
-	events = Event.objects.all().order_by('-created')[:6]
-	categories = Category.objects.all()
-	return render(request, 'index.html', 
-		{'events': events, 'categories': categories})
+    events = Event.objects.all().order_by('-created')[:6]
+    categories = Category.objects.all()
+    return render(request, 'events/index.html',
+        {'events': events, 'categories': categories}
+    )
